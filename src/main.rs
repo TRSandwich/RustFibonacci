03372c066590188f48afe02f647332c5a9ec2b02
laimontr <128290019+TRSandwich@users.noreply.github.com>
2025-06-28 10:59:02 +0100

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     let mut x: i32 = 0;
     let mut y: i32 = 1;
     let mut z: i32 = 0;
-    let i: i32 = 1;
+    let i: i32 = 1; // variable for while loop
 
-    println!("{}", z);
+    println!("{}", z); // prints initial value, 0
 
-    while i == 1 {
-        while z <= 2600 {
+    while i == 1 { // infinite loop
+        while z <= 2600 { // actual loop
             println!("{}", x);
 
             z = x + y;
@@ -17,6 +17,6 @@ fn main() {
             y = z;
         }
         
-        exit(0);
+        exit(0); // exits program
     }
 }
